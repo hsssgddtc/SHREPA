5dcbd72a996e261d85f51e5c032eb57af415c6b8
Ubuntu <kyligence@WebVM.WebVM2381.b3.internal.chinacloudapp.cn>
2017-11-06 01:16:47 +0800

    update abs path of log and daemonid

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ps up `cat mydaemon.pid ` >/dev/null && echo "Program Running" || python Lianjia_Collection_Main_2017.py log
+ps up `cat mydaemon.pid ` >/dev/null && echo "Program Running" || python /home/kyligence/SHREPA/Lianjia_Collection_Main_2017.py log
