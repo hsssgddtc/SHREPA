e0904fe346e22ce39b5eac6c66ee7cc7e3c8391a
sKy <hsssgddtc@gmail.com>
2017-11-06 07:33:35 -0600

    Delete test1.py

--- utilities/test1.py
+++
@@ -1,6 +0,0 @@
-import os
-import time
-
-ISOTIMEFORMAT='%Y%m%d'
-log_file = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"/logs/SHREPA_"+str(time.strftime(ISOTIMEFORMAT))+".log"
-print(log_file)
