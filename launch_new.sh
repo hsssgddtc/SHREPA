3bbd675b8372b84d4b0ca119d4f3898096fb7451
sKy <hsssgddtc@gmail.com>
2017-11-06 23:43:16 +0800

    Update the launch script~

--- launch_new.sh
+++ launch_new.sh
@@ -1,14 +1,9 @@
 #!/bin/bash  
 
-cur_path=`pwd`"/SHREPA_"$1 
-now=$(date +"%Y%m%d")
-output_path=$cur_path"/logs/cron_"$now".output"
-date >> $output_path
-
 funcCreatePath(){
+cur_path=`pwd`"/SHREPA_"$1
 pid_path=$cur_path"/mydaemon.pid"
 code_path=$cur_path"/Lianjia_Collection_Main_2017.py"
-log_path=$cur_path"/"
 if [ $1 = 1 ]
 then district=上海周边,崇明,金山,静安,黄浦,奉贤,闸北
 elif [ $1 = 2 ]
@@ -34,8 +29,7 @@ then district=闵行
 elif [ $1 = 12 ]
 then district=浦东
 fi
-ps up `cat $cur_path"/mydaemon.pid"` >/dev/null && echo "Program Running" || `python $code_path log incre $district` >> $output_path 2>&1
-#echo $district
+ps up `cat $cur_path"/mydaemon.pid"` >/dev/null && echo "Program Running" || `python $code_path log incre $district`
 }
 num=`echo $cur_path | sed 's/[^0-9]//g'`
 funcCreatePath $num
